e84f7a06efdd6309e1a48c9b38a8e5987c92a8d7
Gabriel Martinez <gibbi33@gmail.com>
2016-03-26 13:55:08 -0700

    Updating Chart and Data
    Updated chart, and now working on labels etc. Data sources have been
    updated to newest available.

--- js/elpaso.js
+++ js/elpaso.js
@@ -1,11 +1,12 @@
-var margin = {top: 40, right: 20, bottom: 30, left: 40},
+var margin = {top: 40, right: 20, bottom: 30, left: 60},
     width = 1170 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var formatPercent = d3.format(".%");
+//var formatPercent = d3.format(".%");
+var ourFormat = d3.format(",.2r");
 
 var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
+    .rangeRoundBands([0, width], 1);
 
 var y = d3.scale.linear()
     .range([height, 0]);
@@ -17,16 +18,18 @@ var xAxis = d3.svg.axis()
 var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left")
-    .tickFormat(formatPercent);
+    .tickFormat(ourFormat);
 
+// tooltip
 var tip = d3.tip()
     .attr('class', 'd3-tip')
     .offset([-10, 0])
     .html(function(d) {
-    return "<strong>Name:</strong> <span style='color:orangered'>" + d.tradename + "</span><br /><strong>City:</strong> <span style='color:orangered'>" + d.locationcity + "</span><br /><strong>Reported Tax:</strong> <span style='color:orangered'>" + d.reportedtax + "</span>";
-  });
+    return "<strong>Name:</strong> <span style='color:orangered'>" + d.tradename + "</span><br /><strong>City:</strong> <span style='color:orangered'>" + d.locationcity + "</span><br /><strong>Reported Tax:</strong> <span style='color:orangered'>$" + d.reportedtax + "</span>";
+});
 
-var svg = d3.select("#main-container").append("svg")
+var svg = d3.select("#main-container")
+    .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
@@ -34,16 +37,23 @@ var svg = d3.select("#main-container").append("svg")
 
 svg.call(tip);
 
-d3.csv("../csv/bev02-23.csv", type, function(error, data) {
+d3.csv("../csv/bev01-21.csv", type, function(error, data) {
 
-    x.domain(data.map(function(d) { return d.tradename; }));
-    y.domain([0, d3.max(data, function(d) { return d.reportedtax; })]);
+    // x domain
+    x.domain(data.map(function(d) { return d.locationcity; }));
+    //x.domain(data.map(function(d) { return d.tradename; }));
 
+    // y domain
+    y.domain([0, 20000]);
+    //y.domain([0, d3.max(data, function(d) { return d.reportedtax; })]);
+
+    // x axis
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
 
+    // y axis label
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis)
@@ -52,16 +62,17 @@ d3.csv("../csv/bev02-23.csv", type, function(error, data) {
         .attr("y", 6)
         .attr("dy", ".71em")
         .style("text-anchor", "end")
-        .text("Sales");
+        .text("$ales");
 
+    // bars
     svg.selectAll(".bar")
         .data(data)
         .enter()
         .append("rect")
         .filter(function(d){ return d.locationcountycode ==  "071"; })
         .attr("class", "bar")
-        .attr("x", function(d, i) { return (i*2); })
-        .attr("width", 5)
+        .attr("x", function(d, i) { return (i*2.5); })
+        .attr("width", 4)
         .attr("y", function(d) { return y(d.reportedtax); })
         .attr("height", function(d) { return height - y(d.reportedtax); })
         .on('mouseover', tip.show)
